810f725199153361e52b013ceacd54b3e7acbcc8
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2024-01-25 09:36:10 +0700

    Add proto license file

--- test-tms/main.go
+++ test-tms/main.go
@@ -4,7 +4,7 @@ import "sync"
 
 func main() {
 	var wg sync.WaitGroup
-	for i := 0; i < 500; i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go CallTMSAPIGetCustomer("http://192.168.68.252:5050/api/customer", &wg)
 	}
