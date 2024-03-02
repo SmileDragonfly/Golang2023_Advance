4e51f8aea39688453eb6de0a37d4b1bd562d6a7a
Dat Doan <dat.doantrongk57@gmail.com>
2024-03-02 22:51:22 +0700

    Use sort.Search instead of scan slice then index because Sort.Search use binary tree or quickly methods

--- leetcode/main.go
+++ leetcode/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func lengthOfLongestSubstring(s string) int {
 	if len(s) == 0 {
 		return 0
@@ -21,6 +26,48 @@ func lengthOfLongestSubstring(s string) int {
 	}
 	return maxLength
 }
-func main() {
 
+func findOperation(nums []int, k int) int {
+	sort.Ints(nums)
+	operations := 0
+	for _, v := range nums {
+		if v < k {
+			operations++
+		} else {
+			break
+		}
+	}
+	return operations
+}
+
+func findOperation2(nums []int, k int) int {
+	if len(nums) < 2 {
+		return 0
+	}
+	sort.Ints(nums)
+	operations := 0
+	for {
+		if len(nums) < 2 {
+			return operations
+		}
+		if nums[0] < k || nums[1] < k {
+			operations++
+			add := nums[0]*2 + nums[1]
+			nums = nums[2:]
+			index := sort.Search(len(nums), func(i int) bool { return nums[i] >= add })
+			nums = append(nums, 0)
+			copy(nums[index+1:], nums[index:])
+			nums[index] = add
+		} else {
+			break
+		}
+	}
+	return operations
+}
+
+func main() {
+	nums := []int{21, 35, 90, 51, 27, 19, 57}
+	k := 90
+	oper := findOperation2(nums, k)
+	fmt.Print(oper)
 }
